091734e534217a4399e1a0e6d2d24c41a33cc127
trudova <liubovtrudova@gmail.com>
2020-09-04 16:31:24 -0400

    more css

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,11 @@ export default class Header extends Component {
 <img src ={logo} alt='logo' className='logo'/>
 <Link className='signin-btn'>sign in</Link>
             </div>
+            <div className='header-content'>
+                <h1>See what's next  </h1>
+                <h2> watch anywhere cancel anywhere</h2>
+                <Link className='main-offer-btn'> try it now</Link>
+            </div>
             </div>
         )
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ export default class Main extends Component {
     render() {
         return (
             <div>
-                <h1>the mai component</h1>
                 <Header/>
             </div>
         )
