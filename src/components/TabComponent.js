cc1849720f7b2e83fd9941fa1ec7006ff8544363
trudova <liubovtrudova@gmail.com>
2020-09-08 16:02:54 -0400

    more icons

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -1,13 +1,16 @@
  import React, { Component } from 'react'
  import{Tab, Tabs, TabList, TabPanel} from 'react-tabs'
+ import TabDoor from './tabs_nav/TabDoor'
+ import '../css/TabsNav.css'
+ import TabDevises from './tabs_nav/TabDevises'
  export default class TabComponent extends Component {
      render() {
          return (
              <div>
                  <Tabs>
                      <TabList>
-                         <Tab>Tab Link 1 </Tab>
-                         <Tab>Tab Link 3 </Tab>
+                         <Tab><TabDoor/> </Tab>
+                         <Tab><TabDevises/> </Tab>
                          <Tab>Tab Link 3 </Tab>
                      </TabList>
                  </Tabs>
